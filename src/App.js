d10c104f68d4bfe6c6509ecff0b039d41b0c5db6
alesitom <alemendoza1990@gmail.com>
2022-01-17 15:42:50 -0500

    first commit to gitHub

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Main from './components/Main';
+const App = () => {
+  return (
+    <div className="container">
+      <Main />
+    </div>
+  );
+}
+export default App;
